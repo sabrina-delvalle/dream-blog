b2223c844518984d4d590e7a220af45b9b9eafdb
Sabrina <is.sabrinadelvalle@gmail.com>
2022-12-04 21:22:42 -0400

    Pages Redirection - Updated v0.1

--- front/src/ProtectedRoutes.jsx
+++ front/src/ProtectedRoutes.jsx
@@ -9,6 +9,28 @@ import Cookies from 'js-cookie'
 
 
 function useAuth () {
+
+ /*  let token = null; 
+
+  console.log('before searching the token: ');
+  fetch('http://localhost:5000/tokenvalidation', {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('data retrieve in token for protected!: ', data);
+    if(!data.name){
+      return token = false
+    }
+    return token = true;
+  })
+
+  console.log('the token!! ', token); */
+
   return Cookies.get('userSession')
 }
 
